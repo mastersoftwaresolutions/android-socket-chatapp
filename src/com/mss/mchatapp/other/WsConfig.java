a191976baae4ef42aad32e2a6891f7a1e7f90782
Dinesh Sambial <mss.dineshsambial@gmail.com>
2015-04-18 11:54:40 +0530

    Url changed to server

--- src/com/mss/mchatapp/other/WsConfig.java
+++ src/com/mss/mchatapp/other/WsConfig.java
@@ -3,4 +3,5 @@ package com.mss.mchatapp.other;
 public class WsConfig {
 //	public static final String URL_WEBSOCKET = "ws://192.168.0.105:8080/WebMobileGroupChatServer/chat?name=";
 	public static final String URL_WEBSOCKET = "ws://192.168.0.143:9000/server.php";
+public static final String	URL_WEBSOCKET	= "ws://mastersoftwaretechnologies.com:9000/server.php";
 }
